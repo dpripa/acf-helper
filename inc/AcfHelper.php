3a8814a3703b40c4b36a0d8b1ad6d4213cefce2e
Dmitry Pripa <dmitrypripa@gmail.com>
2025-09-28 02:24:38 +0300

    Refactor ACF plugin dependency registration
    Moved the ACF Pro plugin dependency registration into an 'init' action callback for better timing and encapsulation. This ensures the dependency is required during the WordPress initialization phase.

--- inc/AcfHelper.php
+++ inc/AcfHelper.php
@@ -10,11 +10,16 @@ defined( 'ABSPATH' ) || exit;
 class AcfHelper extends OmgFeature {
 	public function __construct( OmgApp $app ) {
 		parent::__construct( $app );
+		add_action( 'init', $this->init() );
+	}
 
-		$app->dependency()->require_plugin(
-			'acf_pro',
-			'Advanced Custom Fields Pro',
-			'advanced-custom-fields-pro/acf.php'
-		);
+	protected function init(): callable {
+		return function () {
+			$this->app->dependency()->require_plugin(
+				'acf_pro',
+				'Advanced Custom Fields Pro',
+				'advanced-custom-fields-pro/acf.php'
+			);
+		};
 	}
 }
