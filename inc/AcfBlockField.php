b1387408419690e778f03a9467a4b9a14a843f5a
Dmitry Pripa <dmitrypripa@gmail.com>
2025-10-01 04:34:06 +0300

    Simplify AcfBlockField constructor parameters
    Removed unused $get_config and $get_i18n parameters from the AcfBlockField constructor and updated the parent constructor call accordingly.

--- inc/AcfBlockField.php
+++ inc/AcfBlockField.php
@@ -7,8 +7,8 @@ use OmgCore\OmgApp;
 defined( 'ABSPATH' ) || exit;
 
 abstract class AcfBlockField extends OmgFeature {
-	public function __construct( ?OmgApp $app = null, ?callable $get_config = null, ?callable $get_i18n = null ) {
-		parent::__construct( $app, $get_config, $get_i18n );
+	public function __construct( ?OmgApp $app = null ) {
+		parent::__construct( $app );
 		add_action( 'acf/init', $this->register() );
 	}
 
